22f0816b6039f9d4fabd06e0acaf1f82bcff9d7b
“darcy504” <Darcyjdraper@gmail.com>
2021-02-11 18:14:46 -0600

    [WIP] add postgres production

--- app/server/knexfile.js
+++ app/server/knexfile.js
@@ -36,7 +36,15 @@ module.exports = {
     }
   }),
 
-  production: {},
+  production: config({
+    connection: {
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+    },
+  }),
 
 };
 
